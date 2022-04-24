cac49ae0a2423439f050a3496edac976452f7429
KChenO214 <kennychen35@ymail.com>
2022-04-24 17:28:10 -0400

    Commit to Git

--- retrieve.sh
+++ retrieve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Replace ~ with path of project
 #Replace export with your terminal variable declaration
-export CLASSPATH=~/TokenParser/jsoup-1.14.3.jar:$CLASSPATH
-javac ~/TokenParser/src/main/java/tokenize.java
-java ~/TokenParser/src/main/java/tokenize.java "${@:1}"
+export CLASSPATH=~/Text-Search-Engine/jsoup-1.14.3.jar:$CLASSPATH
+javac ~/Text-Search-Engine/src/main/java/tokenize.java
+java ~/Text-Search-Engine/src/main/java/tokenize.java "${@:1}"
